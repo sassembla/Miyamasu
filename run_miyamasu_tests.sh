61f17908a894778b5a834990544f134c1862454d
sassembla <toru.inoue@kissaki.tv>
2016-12-13 02:19:17 +0900

    added test-shell.

---
+++ run_miyamasu_tests.sh
@@ -0,0 +1,11 @@
+# update defines.
+echo -define:CLOUDBUILD > ./Assets/gmcs.rsp
+
+# update date.
+DATE=`date +%Y-%m-%d:%H:%M:%S`
+echo //${DATE} > ./Assets/MiyamasuTestRunner/Editor/Timestamp.cs
+
+/Applications/Unity5.4.2p4/Unity.app/Contents/MacOS/Unity -batchmode -quit -projectPath $(pwd) -executeMethod Miyamasu.MiyamasuTestIgniter.CloudBuildTest
+
+rm ./Assets/gmcs.rsp
+rm ./Assets/MiyamasuTestRunner/Editor/Timestamp.cs
